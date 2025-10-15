6a11449d879134bbf62381597e88feb2405f46cc
unknown <neizanroggie7@gmail.com>
2025-10-15 19:30:58 +0200

    Añadimos Constructor y destructor clase fecha

--- include/Fecha.h
+++ include/Fecha.h
@@ -1,12 +1,34 @@
 #ifndef FECHA_H
 #define FECHA_H
+#include <iostream>
+
+using namespace std;
+//constructor de 3 parametros: dia, mes y año
+//int dia, mes, anio, fecha(d,m,a)
+
+//setfecha(d,m,a)
+//getmes()
+//verfecha()
+//funcion bisiesto
+//f3=f4++ sobrecarga de operador por la izquierda
+//++f4 sobrecarga del operador por la derecha
 
 
 class Fecha
 {
+    int Dia;
+
+    int Mes;
+
+    int Anio;
+
     public:
-        Fecha();
-        virtual ~Fecha();
+        //constructor
+        Fecha(int Dia, int Mes, int Anio);
+
+        //Destructor
+        ~Fecha();
+
 
     protected:
 
--- src/Fecha.cpp
+++ src/Fecha.cpp
@@ -1,11 +1,17 @@
 #include "Fecha.h"
 
-Fecha::Fecha()
-{
-    //ctor
+#include <iostream>
+
+using namespace std;
+
+Fecha::Fecha(int d, int m, int a){
+
+    Dia = a;
+
+    Mes = m;
+
+    Anio = a;
 }
 
-Fecha::~Fecha()
-{
-    //dtor
+Fecha::~Fecha(){
 }
