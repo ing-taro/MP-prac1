943e5d759bd913c95732ed011661e4dc03e9d5fe
unknown <neizanroggie7@gmail.com>
2025-10-29 17:21:50 +0100

    creamos la clase contratotp

---
+++ include/ContratoTP.h
@@ -0,0 +1,23 @@
+#ifndef CONTRATOTP_H
+#define CONTRATOTP_H
+
+#include <iostream> //cin, cout
+#include "Fecha.h"
+#include "Contrato.h"
+
+using namespace std;
+
+class ContratoTP: public Contrato {
+  static int minutosTP;
+  static float precioTP;
+  int minutosHablados;
+  static const float precioExcesoMinutos;
+
+public:
+
+
+};
+
+
+
+#endif // CONTRATOTP_H
---
+++ src/ContratoTP.cpp
@@ -0,0 +1,5 @@
+#include "ContratoTP.h"
+
+int ContratoTP::minutosTP=300;
+float ContratoTP::precioTP=10;
+const float ContratoTP::precioExcesoMinutos=0.15;
